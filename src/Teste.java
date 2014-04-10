de82b802dcd7497e3bb530dce0946ce62678caad
carlos_curbani <carlos_curbani@hotmail.com>
2014-04-10 19:45:04 -0300

    teste de commit

--- src/Teste.java
+++ src/Teste.java
@@ -6,6 +6,7 @@ public class Teste {
 	public static void main(String[] args) {
 		System.out.println("Desenvolvimento de Aplicações Concorrentes e Distribuídas");
 		System.out.println("Teste Tortoise");
+		System.out.println("Vocês estão tudo doidos");
 	}
 
 }
