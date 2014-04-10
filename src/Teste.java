ac59e5a4e609550a25bdbe22c422a92444725bc9
FelipeCorso <felipe.corso@live.com>
2014-04-10 19:21:18 -0300

    Classe de teste

---
+++ src/Teste.java
@@ -0,0 +1,10 @@
+/**
+ * FIXME Deletar esta classe
+ */
+public class Teste {
+
+	public static void main(String[] args) {
+		System.out.println("Desenvolvimento de Aplicações Concorrentes e Distribuídas");
+	}
+
+}
